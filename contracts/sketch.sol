e26b240b357d1481c571469fa92270c74b2cdc4e
JG Carvalho <jgcarvalho93@gmail.com>
2019-05-22 16:00:05 -0300

    begin of implementation

--- contracts/sketch.sol
+++ contracts/sketch.sol
@@ -1,81 +1,88 @@
-pragma solidity 0.5.8;
+pragma solidity 0.5.0;
 
 contract SolidifiedSketch{
 
     enum Severity { Critical, Major, Medium, Minor, Note}
     enum BugStatus { pending, accepted, rejected, negotiation, arbitration}
+    enum ProjectStatus {draft, active, unfunded, closed}
 
     struct Project {
-        address owner,
-        bytes32 infoHash,
-        uint256 Pool,
-        uint256 CriticalValue,
-        uint256 MajorValue,
-        uint256 MediumValue,
-        uint256 MinorValue,
-        uint256 MediumValue,
-        Bug[] bugs
+        uint256 id;
+        address owner;
+        bytes32 infoHash;
+        uint256 Pool;
+        mapping(uint256 => Bug) bugs;
     }
 
     struct Bug {
-        address hunter,
-        Severity severity,
+        address hunter;
+        Severity severity;
     }
 
+    struct Rewards {
+        uint256 Critical;
+        uint256 Major;
+        uint256 Medium;
+        uint256 Minor;
+        uint256 Suggestion;
+    }
+
+    uint256 internal projectCount;
     address public Dai;
-    mapping(address -> uint256) public balances;
-    mapping(address -> mapping(uint256 -> Project)) public projects;
-    mapping(uint -> mapping(uint256 -> Bug)) public bugs;
+    mapping(address => uint256) public balances;
+    mapping(uint256 => Project) public projects; //Owner => Id => Project
+    mapping(uint => mapping(uint256 => Bug)) public bugs; //ProjectId => BugId => Bug
 
-    constructor() {
-
+    constructor() public {
+            projectCount++;
     }
 
 
     /**
             Balance Functions
     **/
-    function deposit() {}
-    function withdraw()
-    function sendTip() {}
+    function deposit() public {}
+    function withdraw() public {}
+    function sendTip() public {}
 
     //Move funds between users and objetcs(Pool, Bug, Arbitration, etc)
-    function sendToPool() {}
-    function sendToBug() {}
-    function sendToArbitration() {}
+    function sendToPool() public {}
+    function sendToBug() public {}
 
     /**
             Contract Posting Functions
     **/
-    function postProject() {}
-    function updateProject() {}
-    function increasePool() {}
-    function pullProject() {}
+    function postProject(bytes32 ipfsHash, uint256 totalPool) public {
+        //Add hash to projects mapping
+        projects[projectCount] = Project(projectCount, msg.sender, ipfsHash, totalPool);
+        //discount from owner balance
+        //emit event
+        //increment project count
+    }
+    function updateProject() public {}
+    function increasePool() public {}
+    function pullProject() public {}
     
     /**
             Bug Functions
     **/
-    function postBug() {}
-    function acceptBug() {}
-    function rejectBug(){}
-    function timeoutAccept() {}
+    function postBug() public {}
+    function acceptBug() public {}
+    function rejectBug() public{}
+    function timeoutAccept() public {}
 
     /**
             Arbitration Functions
     **/
-    function sendToArbitration() {}
-    function commitVote() {}
-    function revealVote(){}
+    function sendToArbitration() public {}
+    function commitVote() public {}
+    function revealVote() public {}
 
     /**
             Administrartive Functions
     **/
-    function upgrade() {}
-    function changeFee() {}
-    function flagBugAsRepetivie() {}
-
-
-
-
+    function upgrade() public {}
+    function changeFee() public {}
+    function flagBugAsRepetivie() public {}
 }
 
