87ddef93e093af8cadc1d41278a6ed32f82df9ed
JG Carvalho <jgcarvalho93@gmail.com>
2019-05-27 14:49:32 -0300

    added openzeppelin and a token mock

--- contracts/SolidifiedBugBounty.sol
+++ contracts/SolidifiedBugBounty.sol
@@ -1,6 +1,11 @@
-pragma solidity 0.5.0;
+pragma solidity 0.5.8;
 
-contract SolidifiedBugBounty{
+import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+contract SolidifiedBugBounty {
+
+    using SafeMath for uint256;
 
     enum Severity { Critical, Major, Medium, Minor, Note}
     enum BugStatus { pending, accepted, rejected, negotiation, arbitration}
@@ -29,39 +34,51 @@ contract SolidifiedBugBounty{
     }
 
     uint256 internal projectCount;
-    address public Dai;
+    address public dai;
     mapping(address => uint256) public balances;
     mapping(uint256 => Project) public projects; //Owner => Id => Project
     mapping(uint => mapping(uint256 => Bug)) public bugs; //ProjectId => BugId => Bug
 
     event ProjectPosted();
+    event Deposit();
 
-    constructor() public {
-            projectCount++;
+    constructor(address _dai) public {
+        dai = _dai; 
+        projectCount++;
     }
-
 
     /**
             Balance Functions
     **/
-    function deposit() public {}
-    function withdraw() public {}
-    function sendTip() public {}
+    function deposit(uint256 _amount) public {
+        require(IERC20(dai).transferFrom(msg.sender, address(this),_amount));
+        balances[msg.sender] = balances[msg.sender].add(_amount);
+        emit Deposit();
+    }
 
-    //Move funds between users and objetcs(Pool, Bug, Arbitration, etc)
-    function sendToPool(address origin, uint256 poolId, uint256 amount) public {}
-    function sendToBug() public {}
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount);
+        require(IERC20(dai).transfer(msg.sender,_amount));
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+        emit Deposit();
+    }
+
+    // function sendTip() public {}
+
+    // //Move funds between users and objetcs(Pool, Bug, Arbitration, etc)
+    // function sendToPool(address origin, uint256 poolId, uint256 amount) public {}
+    // function sendToBug() public {}
 
     /**
             Contract Posting Functions
     **/
     function postProject(bytes32 ipfsHash, uint256 totalPool, uint256[5] memory _rewardsValue) public {
         require(isOrdered(_rewardsValue));
-        require(totalPool >= _rewardsValue[0]); 
+        require(totalPool >= _rewardsValue[0]);
         //Add hash to projects mapping
         projects[projectCount] = Project(projectCount, msg.sender, ipfsHash, totalPool);
         projects[projectCount].rewards[true] = Rewards(_rewardsValue[0],_rewardsValue[1],_rewardsValue[2],_rewardsValue[3],_rewardsValue[4]);
-        sendToPool(msg.sender, projectCount, totalPool);
+        //sendToPool(msg.sender, projectCount, totalPool);
         emit ProjectPosted();
         projectCount++;
     }
@@ -93,7 +110,7 @@ contract SolidifiedBugBounty{
     function flagBugAsRepetivie() public {}
 
     //Helper Functions
-    function isOrdered(uint256[5] memory _arr) public pure returns(bool){
+    function isOrdered(uint256[5] memory _arr) internal pure returns(bool){
         return _arr[0] > _arr[1] && _arr[1] > _arr[2] && _arr[2] > _arr[3] && _arr[3] > _arr[4];
     }
 }
--- test/solidified.js
+++ test/solidified.js
@@ -1,10 +1,23 @@
 const Bounty = artifacts.require("SolidifiedBugBounty");
+const Dai = artifacts.require("MockDai");
 
 contract("Solidified Bug Bounty", accounts => {
-  it("Deploys", async () => {
-    let bb = Bounty.new();
-    console.log(bb.address);
+  let dai,
+    bugBounty = {};
 
-    assert.isTrue(bb.address != 0x0);
+  beforeEach(async () => {
+    dai = await Dai.new();
+  });
+
+  it("Deploys Correctly", async () => {
+    bugBounty = await Bounty.new(dai.address);
+    let daiAddress = await bugBounty.dai.call();
+    assert.equal(daiAddress, dai.address, "Should have correct Dai address");
+  });
+
+  it("Deposit Dai correctly", async () => {
+    bugBounty = await Bounty.new(dai.address);
+    await dai.approve(bugBounty.address, 1000000, { from: accounts[0] });
+    bugBounty.deposit(1000000, { from: accounts[0] });
   });
 });
